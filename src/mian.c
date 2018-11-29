4e69f1709b8897149c9c901283579ad61056eaab
SRI HARSHA KOMMANA <sriharsha.kommana@gmail.com>
2018-11-29 23:37:54 +0530

    Negative

--- src/mian.c
+++ src/mian.c
@@ -1,5 +1,17 @@
 #include "qdbmp.h"
 
+void negative(BMP* input, BMP* output, UINT width, UINT height){
+	UCHAR r, g, b, grey;
+	for (UINT x = 0; x < width; ++x){
+		for (UINT y = 0; y < height; ++y){
+			BMP_GetPixelRGB(input, x, y, &r, &g, &b);
+			BMP_SetPixelRGB(output, x, y, 255 - r, 255 - g, 255 - b);
+		}
+	}
+	return;
+}
+
+
 void main(){
 	BMP *input, *output;
 	UCHAR r, g, b, grey;
@@ -16,8 +28,8 @@ void main(){
 
 	output = BMP_Create(width, height, depth);
 
-	// Use the required function here.
-	
+	negative(input, output, width, height);
+
 	BMP_WriteFile(output, "Negative//swirl_neg.bmp");
 	BMP_CHECK_ERROR(stderr, -2);
 
